12292deedb58895d2fa91e8a8f2daf91aea4f847
dooyounglee <gostbaducking1@nate.com>
2021-11-24 01:38:02 +0900

    final commit

---
+++ src/main/java/com/come/in/ComeinApplication.java
@@ -0,0 +1,13 @@
+package com.come.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/come/in/ComeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.come.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
