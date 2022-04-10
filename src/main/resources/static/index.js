bf8c77dcc2fd15c568cdb995a846c9c58466329c
doo <gostbaducking1@nate.com>
2022-04-11 01:00:25 +0900

    vue 새로고침시 로그인 유지 되도록

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -40,7 +40,6 @@ new Vue({
 			});
 			this.id = '';
 			this.showMenuId = 3;
-			//jsnGet("/exchange/list").then((result) => this.frequencies = result);
 		},
 		logout() {
 			jsnGet("/users/logout").then();
@@ -62,43 +61,20 @@ new Vue({
 		isUser: {
 			handler: 'updateMenus',
 		},
-		/*user: {
-			handler: 'isUser',
-		},*/
 	},
 	created() {
 		jsnGet("/users/checkSession").then((result) => {
 			if(!!result){
+				this.id = result._id;
 				this.user = result;
 				this.showMenuId = 3;
+				this.login();
 			}else{
 				this.user = null;
 				this.frequencies = null;
 				this.showMenuId = 0;
 			}
 		}).catch(e=>console.log(e));
-		/*fetch('http://localhost:8081/exchange/test', null).then(res => {
-			return res.json()
-		}).then(result => {
-			console.log(result);
-			this.frequencies = result;
-		}).catch(e => {
-			console.log(e);
-		})*/
-		//jsn1("/users/list").then((result) => this.users = result);
-		/*let promise1 = jsn1("/users/list");
-		promise1.then((result) => {
-			this.users = result;
-		})*/
-		//jsn("/exchange/test1", this.frequencies1);
-		
-		/*fetch('/exchange/test1', {
-			method: 'POST',
-			body: JSON.stringify({
-				userId: 'userId',
-				type: 'type',
-			})
-		})*/
 	},
 })
 
